21fc72c1468db315d047db25532733f5e2ab23b0
Jouke van der Maas <jouke.vandermaas@gmail.com>
2012-01-10 17:37:29 +0100

    Begin hodex-parser

--- public_html/xml.php
+++ public_html/xml.php
@@ -1,1 +1,43 @@
-<?php phpinfo(); ?>
+<?php
+
+function loadHodexIndex($loc) {
+    $schools = loadSubHodexIndex($loc, "hodexEntity", "hodexDirectoryURL");
+    
+    foreach($schools as $school) {
+        loadHodexSchool($school);
+    }
+}
+function loadHodexSchool($loc) {
+    $degrees = loadSubHodexIndex($loc, "hodexResource", "hodexResourceURL");
+    
+    foreach($degrees as $degree) {
+        loadHodexDegree($degree);
+    }
+}
+function loadHodexDegree($loc) {
+    
+}
+
+function loadSubHodexIndex($loc, $container, $url) {
+    $doc = loadXml($loc);
+    
+    $items = $doc->getElementsByTagName($container);
+    $urls = array();
+    
+    for($i = 0; $i < sizeof($items); $i++) {\
+        $item = $items[$i];
+        $urlElement = $item->getElementByTagName($url);
+        $urls[$i] = $urlElement->item(0)->nodeValue;
+    }
+    return $urls;
+}
+function loadXml($loc) {
+    $doc = new DOMDocument();
+    $doc->load($loc);
+    return $doc;
+}
+
+$loc = "http://www.hodex.nl/hodexDirectory.xml";
+loadHodexIndex($loc);
+
+?>
